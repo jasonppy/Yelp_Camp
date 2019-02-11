886dfc0195828a40c7457f0255cae71ffb933334
Puyuan Peng <peng_puyuan@outlook.com>
2019-02-11 11:23:19 +0800

    use local database

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ var commentRoutes = require("./routes/comments"),
 	authRoutes = require("./routes/index")
 
 
-mongoose.connect("mongodb+srv://myusername:mypassword123@cluster0-vgedy.mongodb.net/yelp_camp?retryWrites=truep",{ useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });
+// mongoose.connect("mongodb+srv://myusername:mypassword123@cluster0-vgedy.mongodb.net/yelp_camp?retryWrites=true",{ useNewUrlParser: true });
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
